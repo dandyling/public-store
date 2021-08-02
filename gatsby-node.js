6ca6765fc650ab4b5239ef18905b1973adacaa30
Dandy Ling <dandyling@gmail.com>
2021-08-02 11:51:09 +0800

    fix: :rocket: Add an exemption for canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,3 +46,24 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+};
